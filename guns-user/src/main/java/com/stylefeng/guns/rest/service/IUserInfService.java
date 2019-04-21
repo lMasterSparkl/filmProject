aa6250d723241c5fb3fc10f2df7992d46ae71127
lMasterSparkl <592712515@qq.com>
2019-04-21 13:02:23 +0800

    first push

---
+++ guns-user/src/main/java/com/stylefeng/guns/rest/service/IUserInfService.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.service;
+
+import com.stylefeng.guns.rest.persistence.model.UserInf;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author zhaolan
+ * @since 2019-04-20
+ */
+public interface IUserInfService extends IService<UserInf> {
+
+}
